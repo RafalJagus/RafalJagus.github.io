c638b20c9fa08feb8e1a1a7da41512c21974456f
Rafał Jaguś <94844926+RafalJagus@users.noreply.github.com>
2022-12-23 11:05:25 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+  const hamburger = document.querySelector(".hamburger");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /* Open & Close Navbar Menu*/
+
+  hamburger.addEventListener("click", () => {
+    document.body.classList.toggle("open");
+    document.body.classList.toggle("stopScrolling");
+  }) 
+
